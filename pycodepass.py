07445605dd54bb71213736d21d30ad17b41f770a
Brice Toueu <guy-brice.toueu@genesis.ci>
2024-11-16 00:37:12 -0800

    Some update

---
+++ pycodepass.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+"""
+This script prints a message indicating how fun the AirBnB console project.
+"""
+
+
+def main():
+    """
+    Main func that prints a message.
+    """
+    print('YAY!! AirBnB console project is FUN!')
+
+
+if __name__ == '__main__':
+    main()
